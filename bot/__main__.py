21e1529ba5289855cd68746cce48b59336dfb123
Groosha <groosha@protonmail.com>
2021-05-18 21:13:02 +0300

    Автоопределение используемых типов апдейтов и получение только их

--- bot/__main__.py
+++ bot/__main__.py
@@ -19,6 +19,24 @@ async def set_bot_commands(bot: Bot):
     await bot.set_my_commands(commands)
 
 
+def get_handled_updates_list(dp: Dispatcher) -> list:
+    """
+    Here we collect only the needed updates for bot based on already registered handlers types.
+    This way Telegram doesn't send unwanted updates and bot doesn't have to proceed them.
+
+    :param dp: Dispatcher
+    :return: a list of registered handlers types
+    """
+    available_updates = (
+        "callback_query_handlers", "channel_post_handlers", "chat_member_handlers",
+        "chosen_inline_result_handlers", "edited_channel_post_handlers", "edited_message_handlers",
+        "inline_query_handlers", "message_handlers", "my_chat_member_handlers", "poll_answer_handlers",
+        "poll_handlers", "pre_checkout_query_handlers", "shipping_query_handlers"
+    )
+    return [item.replace("_handlers", "") for item in available_updates
+            if len(dp.__getattribute__(item).handlers) > 0]
+
+
 async def main():
     # Настройка логирования в stdout
     logging.basicConfig(
@@ -57,7 +75,7 @@ async def main():
     # Запуск поллинга
     # await dp.skip_updates()  # пропуск накопившихся апдейтов (необязательно)
     try:
-        await dp.start_polling()
+        await dp.start_polling(allowed_updates=get_handled_updates_list(dp))
     finally:
         await bot.close()
 
