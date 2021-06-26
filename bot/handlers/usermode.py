4c0b4a06de18b683b99e9d6cdf0fff94c60f62c4
Groosha <groosha@protonmail.com>
2021-06-26 10:10:53 +0300

    Добавлено "переименование чата" в список игнорируемых

--- bot/handlers/usermode.py
+++ bot/handlers/usermode.py
@@ -56,7 +56,8 @@ async def unsupported_types(message: types.Message):
             ContentType.NEW_CHAT_MEMBERS, ContentType.LEFT_CHAT_MEMBER, ContentType.VOICE_CHAT_STARTED,
             ContentType.VOICE_CHAT_ENDED, ContentType.VOICE_CHAT_PARTICIPANTS_INVITED,
             ContentType.MESSAGE_AUTO_DELETE_TIMER_CHANGED, ContentType.NEW_CHAT_PHOTO, ContentType.DELETE_CHAT_PHOTO,
-            ContentType.SUCCESSFUL_PAYMENT, ContentType.PROXIMITY_ALERT_TRIGGERED):
+            ContentType.SUCCESSFUL_PAYMENT, ContentType.PROXIMITY_ALERT_TRIGGERED,
+            ContentType.NEW_CHAT_TITLE):
         await message.reply("К сожалению, этот тип сообщения не поддерживается "
                             "для пересылки от пользователей. Отправь что-нибудь другое.")
 
