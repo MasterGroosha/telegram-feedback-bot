1458368c89279ca24fe761a5a1e3722abe457404
Groosha <groosha@protonmail.com>
2021-08-29 22:58:00 +0300

    Поддержка переключения между вебхуками и поллингом

--- bot/__main__.py
+++ bot/__main__.py
@@ -2,9 +2,12 @@ import asyncio
 import logging
 from os import getenv
 
+from aiohttp import web
 from aiogram import Bot, Dispatcher
+from aiogram.dispatcher.webhook import configure_app
 from aiogram.types import BotCommand
 
+from bot.configreader import load_config, Config
 from bot.handlers.usermode import register_usermode_handlers
 from bot.handlers.adminmode import register_adminmode_handlers
 from bot.handlers.common import register_common_handlers
@@ -46,24 +49,21 @@ async def main():
 
     # Объявление и инициализация объектов бота и диспетчера,
     # а также извлечение переменных окружения с приведением к нужным типам
-    token = getenv("BOT_TOKEN", None)
-    if not token:
+    config: Config = load_config()
+    if not config.bot.token:
         raise ValueError("Не указан токен. Бот не может быть запущен.")
 
-    admin_chat_id = getenv("ADMIN_CHAT_ID", None)
-    if not admin_chat_id:
+    if not config.bot.admin_chat_id:
         raise ValueError("Не указан идентификатор чата для пересылки сообщений. Бот не может быть запущен.")
-    try:
-        admin_chat_id = int(admin_chat_id)
-    except ValueError:
-        raise ValueError(f'Идентификатор "{admin_chat_id}" не является числом. Бот не может быть запущен.')
+    if not isinstance(config.bot.admin_chat_id, int):
+        raise ValueError(f'Идентификатор "{config.bot.admin_chat_id}" не является числом. Бот не может быть запущен.')
 
-    bot = Bot(token=token)
-    bot["admin_chat_id"] = admin_chat_id  # Добавление айдишника к объекту bot
+    bot = Bot(token=config.bot.token)
+    bot["admin_chat_id"] = config.bot.admin_chat_id  # Добавление айдишника к объекту bot
     dp = Dispatcher(bot)
 
     # Регистрация хэндлеров
-    register_adminmode_handlers(dp, admin_chat_id)
+    register_adminmode_handlers(dp, config.bot.admin_chat_id)
     register_common_handlers(dp)
     register_usermode_handlers(dp)
 
@@ -73,16 +73,34 @@ async def main():
     me = await bot.get_me()
     logger.info(f"Starting @{me.username}")
 
-    # Запуск поллинга
-    # await dp.skip_updates()  # пропуск накопившихся апдейтов (необязательно)
-    try:
-        await dp.start_polling(allowed_updates=get_handled_updates_list(dp))
-    finally:
-        await bot.close()
+    # Запуск поллинга или вебхуков
+    if config.app.webhook_enabled:
+        app = web.Application()
+        configure_app(dp, app, config.app.webhook_path)
+        runner = web.AppRunner(app)
+        await runner.setup()
+        await bot.set_webhook(f"https://{config.app.webhook_domain}{config.app.webhook_path}")
+        site = web.TCPSite(runner, config.app.host, config.app.port)
+        print("Starting webhook")
+        try:
+            await site.start()
+            while True:
+                await asyncio.sleep(3600)  # Нужно для поддержания сервера
+        finally:
+            await dp.storage.close()
+            await dp.storage.wait_closed()
+            await bot.session.close()
+            await runner.cleanup()
+    else:
+        try:
+            print("Starting polling")
+            await dp.reset_webhook()
+            # await dp.skip_updates()  # пропуск накопившихся апдейтов (необязательно)
+            await dp.start_polling(allowed_updates=get_handled_updates_list(dp))
+        finally:
+            await dp.storage.close()
+            await dp.storage.wait_closed()
+            await bot.session.close()
 
 
-try:
-    asyncio.run(main())
-except ValueError as ex:
-    logger.error(ex)
-    exit(1)
+asyncio.run(main())
