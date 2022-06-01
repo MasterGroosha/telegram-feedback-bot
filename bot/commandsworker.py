302d9ce730827208808293e2f60f43713f12aca3
vanilla <40017526+plasticsummer@users.noreply.github.com>
2022-06-02 01:07:16 +0600

    Update commandsworker.py

--- bot/commandsworker.py
+++ bot/commandsworker.py
@@ -16,6 +16,7 @@ async def set_bot_commands(bot: Bot):
         BotCommand(command="shadowban", description="Скрытно заблокировать пользователя"),
         BotCommand(command="unban", description="Разблокировать пользователя"),
         BotCommand(command="list_banned", description="Список заблокированных"),
+        BotCommand(command="broadcast", description="Рассылка"),
     ]
     await bot.set_my_commands(
         admin_commands,
