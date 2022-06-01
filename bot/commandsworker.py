5f361de905f76bec32ec82cd1d46b0b2f3bc7371
vanilla <40017526+plasticsummer@users.noreply.github.com>
2022-06-02 01:16:08 +0600

    Update commandsworker.py

--- bot/commandsworker.py
+++ bot/commandsworker.py
@@ -7,7 +7,7 @@ from bot.config_reader import config
 async def set_bot_commands(bot: Bot):
     usercommands = [
         BotCommand(command="help", description="Справка по использованию бота"),
-        BotCommand(command="broadcast", description="Рассылка"),
+        
         
     ]
     await bot.set_my_commands(usercommands, scope=BotCommandScopeDefault())
@@ -18,6 +18,7 @@ async def set_bot_commands(bot: Bot):
         BotCommand(command="shadowban", description="Скрытно заблокировать пользователя"),
         BotCommand(command="unban", description="Разблокировать пользователя"),
         BotCommand(command="list_banned", description="Список заблокированных"),
+        BotCommand(command="broadcast", description="Рассылка"),
     ]
     await bot.set_my_commands(
         admin_commands,
