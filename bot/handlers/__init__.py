894c93296b4ffb24e51c50836158b2e377b3c620
vanilla <40017526+plasticsummer@users.noreply.github.com>
2022-06-01 23:16:32 +0600

    Update __init__.py

--- bot/handlers/__init__.py
+++ bot/handlers/__init__.py
@@ -1,10 +1,9 @@
 from aiogram import Router
-from .private.broadcast import broadcast_command_handler, start_broadcast
 
 
 
 def setup_routers() -> Router:
-    from . import unsupported_reply, admin_no_reply, bans, adminmode, message_edit, usermode
+    from . import unsupported_reply, admin_no_reply, bans, adminmode, message_edit, usermode, broadcast
 
     router = Router()
     router.include_router(unsupported_reply.router)
@@ -13,11 +12,7 @@ def setup_routers() -> Router:
     router.include_router(adminmode.router)
     router.include_router(message_edit.router)
     router.include_router(usermode.router)
-
+    router.include_router(broadcast.router)
+    
     return router
 
-def setup(dp):
-    ...
-    dp.register_message_handler(broadcast_command_handler, commands='broadcast')
-    dp.register_message_handler(start_broadcast, state='broadcast_text', content_types=types.ContentTypes.ANY)
-    logging.info("Handlers are successfully configured")
