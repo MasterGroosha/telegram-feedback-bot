5c601ee5d3fd6f9f7b01dbf4add29fe93a56c323
vanilla <40017526+plasticsummer@users.noreply.github.com>
2022-06-04 17:15:42 +0600

    ads

--- bot/data/db.py
+++ bot/data/db.py
@@ -2,7 +2,7 @@ import sqlite3
 
 class Database:
 	def __init__(self, db_file):
-		self.connection = sqlite3.connect('database.db')
+		self.connection = sqlite3.connect('bot/data/database.db')
 		self.cursor = self.connection.cursor()
 
 
