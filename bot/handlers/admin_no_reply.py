7cb78fda57c22a63f74486495ebff15b416eaaf2
vanilla <40017526+plasticsummer@users.noreply.github.com>
2022-06-06 01:30:40 +0600

    Update admin_no_reply.py

--- bot/handlers/admin_no_reply.py
+++ bot/handlers/admin_no_reply.py
@@ -7,13 +7,13 @@ router = Router()
 router.message.filter(F.chat.id == config.admin_chat_id)
 
 
-@router.message(~F.reply_to_message)
-async def has_no_reply(message: Message):
-    """
-    Хэндлер на сообщение от админа, не содержащее ответ (reply).
-    В этом случае надо кинуть ошибку.
+# @router.message(~F.reply_to_message)
+ # async def has_no_reply(message: Message):
+    # """
+    # Хэндлер на сообщение от админа, не содержащее ответ (reply).
+    # В этом случае надо кинуть ошибку.
 
-    :param message: сообщение от админа, не являющееся ответом на другое сообщение
-    """
-    if message.content_type not in (ContentType.NEW_CHAT_MEMBERS, ContentType.LEFT_CHAT_MEMBER):
-        await message.reply("Это сообщение не является ответом на какое-либо другое!")
+    # :param message: сообщение от админа, не являющееся ответом на другое сообщение
+    # """
+    # if message.content_type not in (ContentType.NEW_CHAT_MEMBERS, ContentType.LEFT_CHAT_MEMBER):
+        # await message.reply("Это сообщение не является ответом на какое-либо другое!")
