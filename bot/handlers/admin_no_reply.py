e073fcc97e32e9af63416023d6b1908d111d16af
vanilla <40017526+plasticsummer@users.noreply.github.com>
2022-06-06 01:36:36 +0600

    Update admin_no_reply.py

--- bot/handlers/admin_no_reply.py
+++ bot/handlers/admin_no_reply.py
@@ -8,15 +8,15 @@ router.message.filter(F.chat.id == config.admin_chat_id)
 
 
 @router.message(~F.reply_to_message)
- async def has_no_reply(message: Message):
+    async def has_no_reply(message: Message):
     """
     Хэндлер на сообщение от админа, не содержащее ответ (reply).
     В этом случае надо кинуть ошибку.
 
     :param message: сообщение от админа, не являющееся ответом на другое сообщение
     """
-    if message.content_type not in (ContentType.NEW_CHAT_MEMBERS, ContentType.LEFT_CHAT_MEMBER):
-     pass
+        if message.content_type not in (ContentType.NEW_CHAT_MEMBERS, ContentType.LEFT_CHAT_MEMBER):
+            pass
     
         
       
