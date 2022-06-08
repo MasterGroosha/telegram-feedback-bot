962eaa07a4a432d036b97907f6281710a6271a07
vanilla <40017526+plasticsummer@users.noreply.github.com>
2022-06-08 17:25:40 +0600

    Update usermode.py

--- bot/handlers/usermode.py
+++ bot/handlers/usermode.py
@@ -51,11 +51,11 @@ async def cmd_sendall(message: Message):
            # text = message.reply_to_message
             users = db.get_users()
             for row in users:
-                if message.reply_to_message.text:
-                    try:
-                        await message.copy_to(row[0])
-                    except Exception as e:
-                        print(e)
+                #if message.reply_to_message.text:
+                try:
+                    await message.copy_to(row[0])
+                except Exception as e:
+                    print(e)
             
                 
                 
