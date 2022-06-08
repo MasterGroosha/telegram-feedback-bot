0b38d26fbe7fb6dae175076fa8a494b34654959e
vanilla <40017526+plasticsummer@users.noreply.github.com>
2022-06-08 17:22:42 +0600

    Update usermode.py

--- bot/handlers/usermode.py
+++ bot/handlers/usermode.py
@@ -48,20 +48,15 @@ async def cmd_start(message: Message):
 async def cmd_sendall(message: Message):
     if message.chat.type == 'private':
         if message.from_user.id == 5181800215:
-            text = message.reply_to_message.text
-            photo = message.reply_to_message_id
+           # text = message.reply_to_message
             users = db.get_users()
             for row in users:
                 if message.reply_to_message.text:
                     try:
-                        await bot.send_message(row[0], text)
+                        await message.copy_to(row[0])
                     except Exception as e:
                         print(e)
-                if message.reply_to_message.photo:
-                    try:
-                        await bot.send_photo(row[0], photo)
-                    except Exception as e:
-                        print(e)
+            
                 
                 
 
