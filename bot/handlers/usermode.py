619be62d95ade3f750ac0077d8d19c1707451669
vanilla <40017526+plasticsummer@users.noreply.github.com>
2022-06-10 05:24:35 +0600

    Update usermode.py

--- bot/handlers/usermode.py
+++ bot/handlers/usermode.py
@@ -209,8 +209,6 @@ async def process_randcat(message: Message):
         'CAACAgIAAxkBAAEEYj5iTKt_yOM7HeXRwZUjVIBGuP8sjgACjhcAAgzYaUjbXjCV4aGCOyME',
         'CAACAgIAAxkBAAEEYkJiTKuuyLA_bXE1xoVc_msY1x1xjAAC3hYAAkAi6Ut6vdE_cd-0-yME'
     ]
-    try:
-        await message.answer_sticker(cats[random.randint(0,len(cats) - 1)])
-    except Exception as e:
-        print(e)
+    await message.answer_sticker(cats[random.randint(0,len(cats) - 1)])
+
 
